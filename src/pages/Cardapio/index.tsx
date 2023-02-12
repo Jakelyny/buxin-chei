25333d3b2bc1687cebe65f9ae4ef086c7c61c4b2
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-12 14:45:50 -0300

    Creating the search

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,20 +1,24 @@
-import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo} from 'assets/logo.svg';
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
 
 export default function Cardapio() {
-    return (
-        <main>
-            <nav className={styles.menu}>
-                <Logo />
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    Impossível não sair de buxin chei
-                </div>
-            </header>
-            <section className={styles.Cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-            </section>
-        </main>
-    )
+  const [busca, setBusca] = useState("");
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+            Impossível não sair de buxin chei
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+      </section>
+    </main>
+  )
 }
