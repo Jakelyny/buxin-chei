647e53be04ae48971fb3fae963b0213e49668771
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-13 20:52:30 -0300

    Handling list sorting and filtering

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,6 +2,7 @@ import cardapio from './itens.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useState, useEffect } from "react";
+import Ordenador from '../Ordenador';
 
 interface Props {
   busca: string,
@@ -11,7 +12,7 @@ interface Props {
 
 export default function Itens(props: Props) {
   const [lista, setLista] = useState(cardapio);
-  const { busca, filtro } = props;
+  const { busca, filtro, ordenador } = props;
 
   function testaBusca(title: string) {
     const regex = new RegExp(busca, 'i');
@@ -23,10 +24,23 @@ export default function Itens(props: Props) {
     return true;
   }
 
+  function ordenar(novaLista: typeof cardapio) {
+    switch(ordenador) {
+      case 'porcao':
+        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas':
+        return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+      case 'preco':
+        return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+      default:
+        return novaLista;
+    }
+  }
+
   useEffect(() => {
     const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-    setLista(novaLista);
-  },[busca, filtro])
+    setLista(ordenar(novaLista));
+  },[busca, filtro, ordenador])
 
   return (
     <div className={styles.itens}>
