c0b9da4e6b92a6f15555cdf289c9bee60a3001ae
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-13 21:09:12 -0300

    Final adjustment

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -17,7 +17,7 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-          A casa do código e da massa
+        Impossível sair de buxin vazio
         </div>
       </header>
       <section className={styles.cardapio}>
