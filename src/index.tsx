674c6de403c8d94ab662f7d9cadfbb0014c2afac
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-21 23:30:10 -0300

    Creating the start component

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Inicio />
   </React.StrictMode>
 );
