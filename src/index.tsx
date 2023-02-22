15d75e24a5dbaae7584e4903ef4cf02e8937b628
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-21 23:51:46 -0300

    Adding routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
+import Router from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>
 );
